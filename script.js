984c9c03ae2ac1f1a12462a675ee598018d30b0b
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-01-16 09:14:53 -0500

    LibreJS compliance because why not ;)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-3.0
 const shareData = {
   title: 'Snapchat++ Installer',
   text: 'Install Snapchat++ easily',
@@ -17,3 +18,4 @@ function buttonClick(element) {
 document.getElementById("share").addEventListener('click', async () => {
   await navigator.share(shareData);
 });
+// @license-end
